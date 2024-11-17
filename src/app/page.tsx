62ddedaac9ee4f4f3e73dd48ecf5d136bf0792c7
artemchs <artemchs123@gmail.com>
2024-11-17 12:15:52 +0100

    feat: :sparkles: set up code for reproduction

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,6 @@
-import { ColorSchemesSwitcher } from "@/components/color-schemes-switcher";
-import {
-  AppShell,
-  AppShellHeader,
-  AppShellMain,
-  Group,
-  Text,
-  Title,
-} from "@mantine/core";
+import { AppShell, AppShellHeader, AppShellMain, Group } from "@mantine/core";
 import Image from "next/image";
+import { Demo } from "./Demo";
 
 export default function Home() {
   return (
@@ -24,41 +17,7 @@ export default function Home() {
         </Group>
       </AppShellHeader>
       <AppShellMain>
-        <Title className="text-center mt-20">
-          Welcome to{" "}
-          <Text
-            inherit
-            variant="gradient"
-            component="span"
-            gradient={{ from: "pink", to: "yellow" }}
-          >
-            Mantine
-          </Text>{" "}
-          +
-          <Text
-            inherit
-            variant="gradient"
-            component="span"
-            gradient={{ from: "blue", to: "green" }}
-          >
-            TailwindCSS
-          </Text>
-        </Title>
-        <Text
-          className="text-center text-gray-700 dark:text-gray-300 max-w-[500px] mx-auto mt-xl"
-          ta="center"
-          size="lg"
-          maw={580}
-          mx="auto"
-          mt="xl"
-        >
-          This starter Next.js project includes a minimal setup for Mantine with
-          TailwindCSS. To get started edit page.tsx file.
-        </Text>
-
-        <div className="flex justify-center mt-10">
-          <ColorSchemesSwitcher />
-        </div>
+        <Demo />
       </AppShellMain>
     </AppShell>
   );
